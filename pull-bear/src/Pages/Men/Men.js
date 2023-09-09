6817195296f26b15aed9d555381710f2ff8cda78
zaurkhalilli <ggencetel274@gmail.com>
2023-09-09 21:39:14 +0400

    some new adds

--- pull-bear/src/Pages/Men/Men.js
+++ pull-bear/src/Pages/Men/Men.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import App from '../../App'
 import Layout from '../../Layout/Layout'
 const Men = () => {
   return (
     <div>
         <Layout>
-        
+
         </Layout>
     </div>
   )
