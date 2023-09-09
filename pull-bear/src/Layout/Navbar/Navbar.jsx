2dd0bf03c9778dbaa54d0bd695e8d7684db150dd
zaurkhalilli <ggencetel274@gmail.com>
2023-09-09 21:43:03 +0400

    some changes

--- pull-bear/src/Layout/Navbar/Navbar.jsx
+++ pull-bear/src/Layout/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from "../Navbar/Navbar.scss"
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
--- pull-bear/src/Pages/Men/Men.js
+++ pull-bear/src/Pages/Men/Men.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import App from '../../App'
 import Layout from '../../Layout/Layout'
+import Navbar from '../../Layout/Navbar/Navbar'
 const Men = () => {
   return (
     <div>
         <Layout>
-
+          <Navbar/>
         </Layout>
     </div>
   )
