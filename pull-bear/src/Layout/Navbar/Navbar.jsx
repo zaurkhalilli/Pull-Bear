4cce68cf42703a32181df8d4f4db18c309fbb184
zaurkhalilli <ggencetel274@gmail.com>
2023-09-09 22:11:03 +0400

    top left added

--- pull-bear/src/Layout/Navbar/Navbar.jsx
+++ pull-bear/src/Layout/Navbar/Navbar.jsx
@@ -6,13 +6,33 @@ const Navbar = () => {
     <div className='navbar'>
     <div className="container">
       <div className="row">
-        <div className="col-xl-4">
+        <div className="col-xl-6">
         <div className="top-left">
-     <a href=""> <p>WOMAN</p> <span>I</span> <h5>Man</h5></a>
+         <a href=""> <p>WOMAN</p> <span>I</span></a>
+         <a href=""><h5>MAN</h5></a>
+     </div>
+     <div className="left">
+      <div className="first">
+        <a href="">NEW</a>
+      </div>
+      <div className="second">
+        <a href="">CLOTHING</a>
+        <br />
+        <a href="">DENIM COLLECTION</a>
+        <br />
+        <a href="">SHOES</a>
+        <br />
+        <a href="">ACCESSORIES</a>
+        <br />
+        <a href="">BAGS/BACKPACKS</a>
+      </div>
+      <div className="third">
+        <a href="">BASICS</a>
+      </div>
      </div>
         </div>
         <div className="col-xl-4"></div>
-        <div className="col-xl-4"></div>
+        <div className="col-xl-2"></div>
       </div>
     </div>
     </div>
