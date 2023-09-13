860cd863ef4c27ef6ee905e18984e12f423dd637
zaurkhalilli <ggencetel274@gmail.com>
2023-09-13 22:42:52 +0400

    new changes added

--- pull-bear/src/Components/MenJeans/Menjeans.jsx
+++ pull-bear/src/Components/MenJeans/Menjeans.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 import styled from "../MenJeans/Menjeans.scss"
 const Menjeans = () => {
   return (
-    <div>
-      
+    <div className='jeans'>
+      <div className="container">
+        <div className="row">
+            <div className="col-xl-2">
+
+            </div>
+            <div className="col-xl-2">
+
+            </div>
+            <div className="col-xl-2">
+
+            </div>
+            <div className="col-xl-2">
+
+            </div>
+            <div className="col-xl-2">
+                <img src="https://static.pullandbear.net/2/photos//2023/I/0/2/p/4684/511/427/03/4684511427_6_1_8.jpg?t=1693225203495&imwidth=750" alt="" />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
--- pull-bear/src/Pages/Men/Men.js
+++ pull-bear/src/Pages/Men/Men.js
@@ -2,11 +2,14 @@ import React from 'react'
 import App from '../../App'
 import Layout from '../../Layout/Layout'
 import Navbar from '../../Layout/Navbar/Navbar'
+import Menjeans from '../../Components/MenJeans/Menjeans'
+import Mencoats from '../../Components/MenCoats/Mencoats'
 const Men = () => {
   return (
     <div>
         <Layout>
-          
+          <Menjeans/>
+          <Mencoats/>
         </Layout>
     </div>
   )
