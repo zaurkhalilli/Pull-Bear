6bd78068132d2ee981768557da6ea85db6bd367e
zaurkhalilli <ggencetel274@gmail.com>
2023-09-14 21:26:40 +0400

    jeans fit added

--- pull-bear/src/Components/MenJeans/Menjeans.jsx
+++ pull-bear/src/Components/MenJeans/Menjeans.jsx
@@ -17,8 +17,15 @@ const Menjeans = () => {
             <a href=""><img src="https://static.pullandbear.net/2/cms/assets/uploads/2X2Skinny_1.jpg?imwidth=300&impolicy=pullandbear-itxmediumhigh&imformat=chrome&ts=20230913021705" alt="" /></a>
             </div>
             <div className="images">
-              <img src="https://static.pullandbear.net/2/photos//2023/I/0/2/p/4684/511/427/03/4684511427_6_1_8.jpg?t=1693225203495&imwidth=750" alt="" />
+            <a href=""> <img src="https://static.pullandbear.net/2/photos//2023/I/0/2/p/4684/511/427/03/4684511427_6_1_8.jpg?t=1693225203495&imwidth=750" alt="" /></a>
             </div>
+           </div>
+           <div className="jeansfit">
+            <a href="">
+              <span>JEANS FIT GUIDE</span>
+              Where you hold the power to select from the most iconic jeans fits of the season. Your style, your choice. Explore our curated fits range.
+            </a>
+            <button>SEE ALL</button>
            </div>
     </div>
   )
