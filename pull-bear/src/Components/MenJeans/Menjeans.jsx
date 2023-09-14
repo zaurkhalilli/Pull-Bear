71bc2b5c38e971cf67eb609bb9568cd7789e4071
zaurkhalilli <ggencetel274@gmail.com>
2023-09-14 21:23:21 +0400

    images added

--- pull-bear/src/Components/MenJeans/Menjeans.jsx
+++ pull-bear/src/Components/MenJeans/Menjeans.jsx
@@ -3,25 +3,23 @@ import styled from "../MenJeans/Menjeans.scss"
 const Menjeans = () => {
   return (
     <div className='jeans'>
-      <div className="container">
-        <div className="row">
-            <div className="col-xl-2">
+           <div className="jeansimages">
+           <div className="images">
             <img src="https://static.pullandbear.net/2/cms/assets/uploads/2X2Baggy_1.jpg?imwidth=300&impolicy=pullandbear-itxmediumhigh&imformat=chrome&ts=20230913021705" alt="" />
             </div>
-            <div className="col-xl-2">
+            <div className="images">
             <img src="https://static.pullandbear.net/2/cms/assets/uploads/2X2Relaxed_1.jpg?imwidth=300&impolicy=pullandbear-itxmediumhigh&imformat=chrome&ts=20230913021705" alt="" />
             </div>
-            <div className="col-xl-2">
+            <div className="images">
             <img src="https://static.pullandbear.net/2/cms/assets/uploads/2X2Slim_1.jpg?imwidth=300&impolicy=pullandbear-itxmediumhigh&imformat=chrome&ts=20230913021705" alt="" />
             </div>
-            <div className="col-xl-2">
+            <div className="images">
             <img src="https://static.pullandbear.net/2/cms/assets/uploads/2X2Skinny_1.jpg?imwidth=300&impolicy=pullandbear-itxmediumhigh&imformat=chrome&ts=20230913021705" alt="" />
             </div>
-            <div className="col-xl-2">
-                <img src="https://static.pullandbear.net/2/photos//2023/I/0/2/p/4684/511/427/03/4684511427_6_1_8.jpg?t=1693225203495&imwidth=750" alt="" />
+            <div className="images">
+              <img src="https://static.pullandbear.net/2/photos//2023/I/0/2/p/4684/511/427/03/4684511427_6_1_8.jpg?t=1693225203495&imwidth=750" alt="" />
             </div>
-        </div>
-      </div>
+           </div>
     </div>
   )
 }
